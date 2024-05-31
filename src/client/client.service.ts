941e73a4851fe616016ac05d5fd95aae69405b72
Леонид Иванов <ivanov.l@robotkarl.com>
2024-06-01 02:07:11 +0800

    s

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -143,119 +143,121 @@ export class ClientService {
 
     const demoMode = res;
 
-    if (res.payment_status !== "success") {
+    if (res.payment_status === "success" || res.payment_status === "order_approved" ) {
+      const findPayment = await this.prisma.payment.findFirst({
+        where: {
+          paymentToken: res.order_num
+        }
+      })
+
+      if(findPayment){
+        throw new Error("Payment Already Registered");
+      }
+
+      // Создаем нового клиента
+      const client = await this.prisma.client.create({
+        data: {
+          email: res.customer_email,
+          phone: res.customer_phone,
+          typeProduct: res.customer_extra
+        }
+      });
+
+      // const removedToken = this.deleteToken(clientData.id);
+      //
+      // if (!removedToken) {
+      //   throw new Error("Token not deleted");
+      // }
+
+      // Находим первого студента с включенным queue
+      const studentToAssign = await this.prisma.user.findFirst({
+        where: {
+          role: "student",
+          queue: true,
+          telegram: {
+            not: null // Фильтр для учета только тех, у кого есть telegram
+          },
+          atv: true
+        },
+        orderBy: {
+          createdAt: "asc" // Сортируем по дате создания для определения порядка
+        }
+      });
+
+      if (!studentToAssign) {
+        throw new Error("No students available");
+      }
+
+
+      // Присваиваем клиента студенту и выключаем queue
+      const updatedStudent = await this.prisma.user.update({
+        where: {
+          id: studentToAssign.id
+        },
+        data: {
+          clients: {
+            connect: {
+              id: client.id
+            }
+          },
+          queue: false // Выключаем queue
+        }
+      });
+
+      // Проверяем, является ли студент последним в списке
+      const isLastStudent = await this.prisma.user.findFirst({
+        where: {
+          role: "student",
+          id: {
+            not: studentToAssign.id // Исключаем текущего студента
+          },
+          telegram: {
+            not: null // Фильтр для учета только тех, у кого есть telegram
+          },
+          atv: true,
+          queue: true // Ищем других студентов с включенным queue
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }) === null;
+
+      // Если это последний студент, включаем queue для всех студентов
+      if (isLastStudent) {
+        await this.prisma.user.updateMany({
+          where: {
+            role: "student"
+          },
+          data: {
+            queue: true // Включаем queue
+          }
+        });
+      }
+
+      const newClient = await this.prisma.client.findFirst({
+        where: { id: client.id }
+      });
+
+      const payment = await this.prisma.payment.create({
+        data: {
+          json: JSON.stringify({
+            ...res,
+            client: newClient,
+            student: {
+              telegram: updatedStudent.telegram,
+              id: updatedStudent.id
+            }
+          }),
+          paymentToken: res.order_num,
+          customerEmail: newClient.email
+        }
+      });
+
+      return payment;
+    }else{
       throw new Error("Bad Payment");
     }
 
-    const findPayment = await this.prisma.payment.findFirst({
-      where: {
-        paymentToken: res.order_num
-      }
-    })
 
-    if(findPayment){
-      throw new Error("Payment Already Registered");
-    }
-
-    // Создаем нового клиента
-    const client = await this.prisma.client.create({
-      data: {
-        email: res.customer_email,
-        phone: res.customer_phone,
-        typeProduct: res.customer_extra
-      }
-    });
-
-    // const removedToken = this.deleteToken(clientData.id);
-    //
-    // if (!removedToken) {
-    //   throw new Error("Token not deleted");
-    // }
-
-    // Находим первого студента с включенным queue
-    const studentToAssign = await this.prisma.user.findFirst({
-      where: {
-        role: "student",
-        queue: true,
-        telegram: {
-          not: null // Фильтр для учета только тех, у кого есть telegram
-        },
-        atv: true
-      },
-      orderBy: {
-        createdAt: "asc" // Сортируем по дате создания для определения порядка
-      }
-    });
-
-    if (!studentToAssign) {
-      throw new Error("No students available");
-    }
-
-
-    // Присваиваем клиента студенту и выключаем queue
-    const updatedStudent = await this.prisma.user.update({
-      where: {
-        id: studentToAssign.id
-      },
-      data: {
-        clients: {
-          connect: {
-            id: client.id
-          }
-        },
-        queue: false // Выключаем queue
-      }
-    });
-
-    // Проверяем, является ли студент последним в списке
-    const isLastStudent = await this.prisma.user.findFirst({
-      where: {
-        role: "student",
-        id: {
-          not: studentToAssign.id // Исключаем текущего студента
-        },
-        telegram: {
-          not: null // Фильтр для учета только тех, у кого есть telegram
-        },
-        atv: true,
-        queue: true // Ищем других студентов с включенным queue
-      },
-      orderBy: {
-        createdAt: "asc"
-      }
-    }) === null;
-
-    // Если это последний студент, включаем queue для всех студентов
-    if (isLastStudent) {
-      await this.prisma.user.updateMany({
-        where: {
-          role: "student"
-        },
-        data: {
-          queue: true // Включаем queue
-        }
-      });
-    }
-
-    const newClient = await this.prisma.client.findFirst({
-      where: { id: client.id }
-    });
-
-    const payment = await this.prisma.payment.create({
-      data: {
-        json: JSON.stringify({
-          ...res,
-          client: newClient,
-          student: {
-            telegram: updatedStudent.telegram,
-            id: updatedStudent.id
-          }
-        }),
-        paymentToken: res.order_num,
-        customerEmail: newClient.email
-      }
-    });
-
-    return payment;
   }
 }
